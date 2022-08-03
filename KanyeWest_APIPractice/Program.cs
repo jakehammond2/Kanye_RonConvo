1414c2e6bc93a157c594ec48fe93c5355834eb6e
Jake Hammond <jakehammond226@gmail.com>
2022-08-03 14:32:56 -0500

    1st commit

---
+++ KanyeWest_APIPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+
+var kanyeURL = "https://api.kanye.rest/";
+var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+for (int i = 0; i < 5; i++)
+{
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result; //Json object
+    var ronResponse = client.GetStringAsync(ronURL).Result;
+
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();  //parsing kanye response //converting kanye repsonse
+    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[',' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+    Console.WriteLine($"Kanye: {kanyeQuote}");
+    Console.WriteLine($"Ron Swanson: {ronQuote}");
+    Console.WriteLine();
+}
